96de2a50b3cd9f1fe89f316448d083fc67c0f5f6
Romulo <rgcostabox@gmail.com>
2019-04-23 19:26:36 -0500

    Change package from servlet to controller

--- src/main/java/controller/AllUserServlet.java
+++ src/main/java/controller/AllUserServlet.java
@@ -1,4 +1,4 @@
-package servlet;
+package controller;
 
 import dao.UserDataAccess;
 import model.User;
--- src/main/java/controller/TeamController.java
+++ src/main/java/controller/TeamController.java
@@ -1,4 +1,4 @@
-package servlet;
+package controller;
 
 import dao.TeamDataAccess;
 import model.Team;
